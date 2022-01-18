9d3bf45bfce42030b7b31ae291e74b5e02c6ac0a
leon <25666053+ployt0@users.noreply.github.com>
2022-01-18 13:29:22 +0000

    Initial commit

---
+++ tests/test_update_mod_date.py
@@ -0,0 +1,224 @@
+import datetime
+import subprocess
+from unittest.mock import patch, mock_open, sentinel, Mock, call
+
+import pytest
+
+from update_mod_date import main, replace_footer, cmp_and_overwrite_mtime, find_modified_html_files, git_add_updated, \
+    check_changes, validate_html
+
+SAMPLE_HTML = '''\
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Sentinel.Site</title>
+</head>
+<body>
+<header>
+Branding
+</header>
+<nav>
+<h3>Posts:</h3>
+<ul>
+<li>Foo</li>
+<li>Bar</li>
+</ul>
+</nav>
+<footer>
+<small>published: 18/Jan/2019 modified: 19/Jan/2019</small>
+</footer>
+</body>
+</html>
+'''
+
+EXPECTED_HTML = '''\
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Sentinel.Site</title>
+</head>
+<body>
+<header>
+Branding
+</header>
+<nav>
+<h3>Posts:</h3>
+<ul>
+<li>Foo</li>
+<li>Bar</li>
+</ul>
+</nav>
+<footer>
+<hr>
+<small>updated 31/Dec/2021</small>
+</footer>
+</body>
+</html>
+'''
+
+MINIMAL_HTML = '''\
+<body>call me .html</body>
+'''
+
+EXPECTED_MINIMAL_HTML = '''\
+<body>call me .html<footer>
+<hr>
+<small>updated 31/Dec/2021</small>
+</footer>
+</body>
+'''
+
+BAD_HTML = '''\
+<em>Glorified text</em>
+'''
+
+
+@patch("update_mod_date.git_add_updated")
+@patch("update_mod_date.replace_footer", return_value="SENTINEL HTML")
+@patch("builtins.open", new_callable=mock_open, read_data=SAMPLE_HTML)
+def test_cmp_and_overwrite_mtime(mocked_open, mock_replace_footer, mock_git_add_updated):
+    cmp_and_overwrite_mtime(sentinel.file_name, '31/Dec/2021', False)
+    mocked_open.assert_has_calls([
+        call(sentinel.file_name, encoding='utf8'),
+        call(sentinel.file_name, 'w', encoding='utf8')
+    ], any_order=True)
+    mock_replace_footer.assert_called_once_with(SAMPLE_HTML, '31/Dec/2021', sentinel.file_name)
+    mocked_open.return_value.write.assert_called_once_with('SENTINEL HTML')
+    mock_git_add_updated.assert_called_once_with(sentinel.file_name)
+
+
+@patch("update_mod_date.git_add_updated")
+@patch("update_mod_date.replace_footer", return_value="SENTINEL HTML")
+@patch("builtins.open", new_callable=mock_open, read_data=SAMPLE_HTML)
+def test_cmp_and_overwrite_mtime_with_bkup(mocked_open, mock_replace_footer, mock_git_add_updated):
+    cmp_and_overwrite_mtime("original_file_name", '31/Dec/2021', True)
+    mocked_open.assert_has_calls([
+        call("original_file_name", encoding='utf8'),
+        call("original_file_name", 'w', encoding='utf8'),
+        call("original_file_name" + ".bkup", 'w', encoding='utf8')
+    ], any_order=True)
+    mock_replace_footer.assert_called_once_with(SAMPLE_HTML, '31/Dec/2021', "original_file_name")
+    mocked_open.return_value.write.assert_has_calls([
+        call('SENTINEL HTML'),
+        call(SAMPLE_HTML),
+    ])
+    mock_git_add_updated.assert_called_once_with("original_file_name")
+
+
+@patch("update_mod_date.git_add_updated")
+@patch("update_mod_date.replace_footer", return_value=EXPECTED_HTML)
+@patch("builtins.open", new_callable=mock_open, read_data=EXPECTED_HTML)
+def test_cmp_and_overwrite_mtime_redundant(mocked_open, mock_replace_footer, mock_git_add_updated):
+    cmp_and_overwrite_mtime(sentinel.file_name, '31/Dec/2021', True)
+    mocked_open.assert_called_once_with(sentinel.file_name, encoding='utf8')
+    mock_replace_footer.assert_called_once_with(EXPECTED_HTML, '31/Dec/2021', sentinel.file_name)
+    mocked_open.return_value.write.assert_not_called()
+    mock_git_add_updated.assert_not_called()
+
+
+@patch("update_mod_date.git_add_updated")
+@patch("update_mod_date.replace_footer", side_effect=RuntimeError("Test only"))
+@patch("builtins.open", new_callable=mock_open, read_data=SAMPLE_HTML)
+def test_validate_html(mocked_open, mock_replace_footer, mock_git_add_updated):
+    with pytest.raises(RuntimeError):
+        validate_html(sentinel.file_name)
+    mocked_open.assert_called_once_with(sentinel.file_name, encoding='utf8')
+    mock_replace_footer.assert_called_once_with(SAMPLE_HTML, 'unused', sentinel.file_name)
+    mocked_open.return_value.write.assert_not_called()
+    mock_git_add_updated.assert_not_called()
+
+
+def test_replace_footer():
+    updated_html = replace_footer(SAMPLE_HTML, "31/Dec/2021", "good.html")
+    assert updated_html == EXPECTED_HTML
+
+
+def test_replace_footer_redundant():
+    updated_html = replace_footer(EXPECTED_HTML, "31/Dec/2021", "good.html")
+    assert updated_html == EXPECTED_HTML
+
+
+def test_replace_footer_minimal_html():
+    updated_html = replace_footer(MINIMAL_HTML, "31/Dec/2021", "minimal.html")
+    assert updated_html == EXPECTED_MINIMAL_HTML
+
+
+def test_replace_footer_bad_html():
+    with pytest.raises(RuntimeError):
+        replace_footer(BAD_HTML, "31/Dec/2021", "bad.html")
+
+
+@patch("update_mod_date.subprocess.run", return_value=Mock(
+    subprocess.CompletedProcess, returncode=0,
+    stdout=b"M       file1.txt\nM       file2.html\nM       file3.html\n"))
+def test_find_modified_html_files(mock_run):
+    modified_html_files = find_modified_html_files()
+    mock_run.assert_called_once_with("git diff --name-status --cached".split(), capture_output=True)
+    assert modified_html_files == ["file2.html", "file3.html"]
+
+
+@patch("update_mod_date.subprocess.run", return_value=Mock(
+    subprocess.CompletedProcess, returncode=0,
+    stdout=b"M       file1.txt\nD       file2.html\nM       file3.html\n"))
+def test_find_modified_html_files_not_deleted(mock_run):
+    modified_html_files = find_modified_html_files()
+    mock_run.assert_called_once_with("git diff --name-status --cached".split(), capture_output=True)
+    assert modified_html_files == ["file3.html"]
+
+
+@patch("update_mod_date.subprocess.run", return_value=Mock(
+    subprocess.CompletedProcess, returncode=0,
+    stdout=b"M       file1.txt\nM       file2.txt\nM       file3.txt\n"))
+def test_find_modified_files_all_text(mock_run):
+    modified_html_files = find_modified_html_files()
+    mock_run.assert_called_once_with("git diff --name-status --cached".split(), capture_output=True)
+    assert modified_html_files == []
+
+
+@patch("update_mod_date.subprocess.run", return_value=Mock(
+    subprocess.CompletedProcess, returncode=1,
+    stderr=b"This is a test", stdout=b"M       file1.html\n"))
+def test_find_modified_html_files_git_error(mock_run):
+    with pytest.raises(RuntimeError):
+        find_modified_html_files()
+    mock_run.assert_called_once_with("git diff --name-status --cached".split(), capture_output=True)
+
+
+@patch("update_mod_date.subprocess.run", return_value=Mock(
+    subprocess.CompletedProcess, returncode=0))
+def test_git_add_updated(mock_run):
+    git_add_updated(sentinel.changed_file)
+    mock_run.assert_called_once_with(["git", "add", sentinel.changed_file], capture_output=True)
+
+
+@patch("update_mod_date.subprocess.run", return_value=Mock(
+    subprocess.CompletedProcess, returncode=1, stderr=b"This is a test"))
+def test_git_add_updated_git_error(mock_run):
+    with pytest.raises(RuntimeError):
+        git_add_updated(sentinel.changed_file)
+    mock_run.assert_called_once_with(["git", "add", sentinel.changed_file], capture_output=True)
+
+
+@patch("update_mod_date.cmp_and_overwrite_mtime", return_value=["sentinel1.txt", "sentinel2.txt"])
+@patch("update_mod_date.find_modified_html_files", return_value=["sentinel1.txt", "sentinel2.txt"])
+@patch("update_mod_date.datetime", spec=datetime)
+def test_main(mock_datetime, mock_find_modified_html_files, mock_cmp_and_overwrite_mtime):
+    mock_datetime.utcnow = Mock(
+        side_effect=[datetime.datetime(2021, 12, 31, 12, 30, 00)] * 2)
+    main()
+    mock_find_modified_html_files.assert_called_once_with()
+    mock_cmp_and_overwrite_mtime.assert_has_calls([
+        call("sentinel1.txt", '31/Dec/2021', False),
+        call("sentinel2.txt", '31/Dec/2021', False)
+    ])
+
+
+@patch("update_mod_date.validate_html", return_value=["sentinel1.txt", "sentinel2.txt"])
+@patch("update_mod_date.find_modified_html_files", return_value=["sentinel1.txt", "sentinel2.txt"])
+def test_check_changes(mock_find_modified_html_files, mock_validate_html):
+    check_changes()
+    mock_find_modified_html_files.assert_called_once_with()
+    mock_validate_html.assert_has_calls([
+        call("sentinel1.txt"),
+        call("sentinel2.txt")
+    ])
---
+++ update_mod_date.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+"""
+Used by the pre-commit git hook to update the modification time of html files.
+Still inferior to a dedicated CMS because we don't distinguish modification of
+structure, formatting, and style etc from content.
+
+Copying this very file .git/hooks/pre-commit *may* work. What I do is write
+that file as:
+
+>    #!/bin/sh
+>
+>    ./update_mod_date.py
+"""
+import os
+import subprocess
+from datetime import datetime
+from typing import List
+
+DATE_TIME_FMT = "%y%m%d %H:%M:%S"
+
+
+def validate_html(file_to_check: str):
+    with open(file_to_check, encoding="utf8") as f:
+        replace_footer(f.read(), "unused", file_to_check)
+
+
+def cmp_and_overwrite_mtime(changed_file: str, mod_time_stamp: str, save_original: bool) -> None:
+    """
+    :param changed_file: single file to update with mod_time_stamp.
+    :param mod_time_stamp: to place in footer of file.
+    :param save_original: makes a backup (appending ".bkup") of any file being modified.
+        I'd really rather not have this option; it makes more files to manage,
+        but it helps whilst this script earns trust.
+    """
+    with open(changed_file, encoding="utf8") as f:
+        html_contents = f.read()
+    updated_html = replace_footer(html_contents, mod_time_stamp, changed_file)
+    if updated_html == html_contents:
+        # No sense in changing the timestamp or re-adding if nothing changed.
+        return
+    print("Adding today's date (in footer) to an updated {}".format(changed_file))
+    with open(changed_file, "w", encoding="utf8") as f:
+        f.write(updated_html)
+    if save_original:
+        print("Saving original as {}.bkup".format(changed_file))
+        with open(changed_file + ".bkup", "w", encoding="utf8") as f:
+            f.write(html_contents)
+    git_add_updated(changed_file)
+
+
+def replace_footer(html_text: str, mod_time_stamp: str, source_name: str) -> str:
+    if "<footer>" not in html_text and "</body>" not in html_text:
+        raise RuntimeError("Malformed HTML. {} requires at least a closing </body> tag.".format(source_name))
+    if "<footer>" not in html_text:
+        html_text = html_text.replace("</body>", "<footer></footer>\n</body>")
+    pieces1 = html_text.split("<footer>")
+    aft = pieces1[1]
+    pieces2 = aft.split("</footer>")
+    if "updated {}".format(mod_time_stamp) in pieces2[0]:
+        return html_text
+    footer = "<footer>\n<hr>\n<small>updated {}</small>\n</footer>".format(mod_time_stamp)
+    return pieces1[0] + footer + pieces2[1]
+
+
+def find_modified_html_files() -> List[str]:
+    staged_files = subprocess.run("git diff --name-status --cached".split(), capture_output=True)
+    if staged_files.returncode != 0:
+        raise RuntimeError(staged_files.stderr.decode())
+    extant_modded_html_files = []
+    for line in staged_files.stdout.decode().strip().split("\n"):
+        if line.startswith("D"):
+            continue
+        if "." in line and line.split(".")[-1] in ["html", "htm"]:
+            extant_modded_html_files.append(line[1:].strip())
+    return extant_modded_html_files
+
+
+def git_add_updated(changed_file: str) -> None:
+    re_add_completed = subprocess.run(["git", "add", changed_file], capture_output=True)
+    if re_add_completed.returncode != 0:
+        raise RuntimeError(re_add_completed.stderr.decode())
+
+
+def main():
+    mod_time_stamp = datetime.utcnow().strftime("%d/%b/%Y")
+    changed_files = find_modified_html_files()
+    print("Checking times of: {}".format(changed_files))
+    for changed_file in changed_files:
+        cmp_and_overwrite_mtime(changed_file, mod_time_stamp, False)
+
+
+def check_changes():
+    changed_files = find_modified_html_files()
+    for file_to_check in changed_files:
+        validate_html(file_to_check)
+
+#     <meta name="DC.Date" content="en" />
+# Consider creation date, maybe
+
+
+if __name__ == "__main__":
+    # Test all the files before proceeding to update the modification time.
+    check_changes()
+    main()
